6288e9f12f480c707d23e2a8f25577816deafca5
Phil Hope <60958961+lrdswag@users.noreply.github.com>
2021-10-29 09:54:02 +1000

    Update EmployeeForm.js
    Updated EmployeeForm.js. Form state now working with the onchange handlers. 
    TODO: Getting 401 Unauthorized from API. Possibly sending incorrect value names to back end?

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,87 +1,103 @@
 import React, { useState } from "react";
 
-export default function EmployeeForm(props) {
-  const [updatedInput, setUpdatedInput] = useState({});
+class EmployeeForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let employees_idNumber = props.employees_idNumber;
-  let firstName = props.firstName;
-  let department = props.department;
-  let clockInNum = props.clockInNum;
-  let phone_number = props.phone_number;
-  let is_manager = props.is_manager;
-  let lastName = props.lastName;
-  let DOB = props.DOB;
-  let email = props.email;
+    this.state = {
+      employees_idNumber: this.props.employees_idNumber,
+      firstName: props.firstName,
+      department: props.department,
+      clock_Number: props.clock_Number,
+      phone_number: props.phone_number,
+      is_manager: props.is_manager,
+      lastName: props.lastName,
+      dateOfBirth: props.dateOfBirth,
+      email: props.email,
+      closer: props.closer
+    };
 
-  const employeeIdHandler = () => {
-    setUpdatedInput({
-      ...updatedInput,
-      employees_idNumber: props.employees_idNumber,
+    this.employeeIdHandler = this.employeeIdHandler.bind(this);
+    this.firstNameUpdateChangeHandler = this.firstNameUpdateChangeHandler.bind(this);
+    this.lastNameUpdateChangeHandler = this.lastNameUpdateChangeHandler.bind(this);
+    this.dateUpdateChangeHandler = this.dateUpdateChangeHandler.bind(this);
+    this.emailUpdateChangeHandler = this.emailUpdateChangeHandler.bind(this);
+    this.phoneNumberUpdateChangeHandler = this.phoneNumberUpdateChangeHandler.bind(this);
+    this.clockNumberUpdateChangeHandler = this.clockNumberUpdateChangeHandler.bind(this);
+    this.departmentUpdateChangeHandler = this.departmentUpdateChangeHandler.bind(this);
+    this.is_managerUpdateChangeHandler = this.is_managerUpdateChangeHandler.bind(this);
+    this.updateMyDetailsViewForm = this.updateMyDetailsViewForm.bind(this);
+  }
+
+
+  employeeIdHandler(event) {
+    this.setState({
+      employees_idNumber: event.target.value,
     });
     console.log(1);
   };
-  const firstNameUpdateChangeHandler = (event) => {
-    setUpdatedInput({
+
+  firstNameUpdateChangeHandler(event) {
+    this.setState({
       firstName: event.target.value,
     });
-    console.log(updatedInput);
   };
-  const lastNameUpdateChangeHandler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
-      lastName: event.target.value,
+
+  lastNameUpdateChangeHandler(event) {
+    this.setState({
+      lastName: event.target.lastName,
     });
     console.log(3);
   };
-  const dateUpdateChangeHandler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
-      DOB: event.target.value,
+
+  dateUpdateChangeHandler(event) {
+    this.setState({
+      dateOfBirth: event.target.dateOfBirth,
     });
     console.log(4);
   };
-  const emailUpdateChangeHanndler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
-      email: event.target.value,
+
+  emailUpdateChangeHandler(event) {
+    this.setState({
+      email: event.target.email,
     });
   };
-  const phoneNumberUpdateChangeHandler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
-      phone_number: event.target.value,
+
+  phoneNumberUpdateChangeHandler(event) {
+    this.setState({
+      phone_number: event.target.phone_number,
     });
   };
-  const clockNumberUpdateChangeHandler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
-      clockInNum: event.target.value,
+
+  clockNumberUpdateChangeHandler(event) {
+    this.setState({
+      clock_Number: event.target.value,
     });
-  };
-  const departmentUpdateChangeHandler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
+  }
+
+  departmentUpdateChangeHandler(event) {
+    this.setState({
       department: event.target.value,
     });
   };
-  const is_managerUpdateChangeHandler = (event) => {
-    setUpdatedInput({
-      ...updatedInput,
+
+  is_managerUpdateChangeHandler(event) {
+    this.setState({
       is_manager: event.target.value,
     });
   };
 
-  function updateMyDetailsViewForm(evt) {
+   updateMyDetailsViewForm(evt) {
     const updatedEmployeeData = {
-      employees_idNumber: employees_idNumber,
-      firstName: firstName,
-      department: department,
-      clockInNum: clockInNum,
-      phone_number: phone_number,
-      is_manager: is_manager,
-      lastName: lastName,
-      DOB: DOB,
-      email: email,
+      employees_idNumber: this.state.employees_idNumber,
+      firstName: this.state.firstName,
+      department: this.state.department,
+      clockInNum: this.state.clock_Number,
+      phone_number: this.state.phone_number,
+      is_manager: this.state.is_manager,
+      lastName: this.state.lastName,
+      DOB: this.state.dateOfBirth,
+      email: this.state.email
     };
     console.log(updatedEmployeeData);
     evt.preventDefault(evt);
@@ -92,25 +108,26 @@ export default function EmployeeForm(props) {
       credentials: "include",
     }).then(function (headers) {
       if (headers.status === 201) {
-        props.showAlert("success", "Employee added");
-        props.closer();
+        // this.showAlert("success", "Employee added");
+        // this.closer();
       }
       if (headers.status === 401) {
-        props.showAlert("error", "This action didn't work");
-        props.closer();
+        // this.showAlert("error", "This action didn't work");
+        // this.closer();
       }
       if (headers.status === 429) {
-        props.showAlert("error", "This action didn't work");
-        props.closer();
+        // this.showAlert("error", "This action didn't work");
+        // this.closer();
       }
       if (headers.status === 500) {
-        props.showAlert("error", "This action didn't work");
-        props.closer();
+        // this.showAlert("error", "This action didn't work");
+        // this.closer();
       }
     });
   }
 
-  return (
+  render() {
+    return (
     <div>
       <section className="modal-card-body">
         <form>
@@ -118,13 +135,12 @@ export default function EmployeeForm(props) {
             <label className="label">Employee Id</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                readOnly
-                defaultValue={props.employees_idNumber}
-                id={props.id}
-                name="employees_idNumber"
-                type="text"
-                onChange={employeeIdHandler}
-                className="is-size-3"
+                  defaultValue={this.employees_idNumber}
+                  id={this.id}
+                  name="employees_idNumber"
+                  type="text"
+                  onChange={this.employeeIdHandler}
+                  className="is-size-3"
               />
             </div>
           </div>
@@ -132,15 +148,15 @@ export default function EmployeeForm(props) {
             <label className="label">First Name</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                defaultValue={props.firstName}
-                onChange={firstNameUpdateChangeHandler}
-                id="first_name_admin_view"
-                className="input is-link"
-                type="text"
-                placeholder="Employees first name"
-                name="firstName"
-                required
-                pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+"
+                  defaultValue={this.state.firstName}
+                  onChange={this.firstNameUpdateChangeHandler}
+                  id="first_name_admin_view"
+                  className="input is-link"
+                  type="text"
+                  placeholder="Employees first name"
+                  name="firstName"
+                  required
+                  pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+"
               />
               <span className="icon is-small is-left">
                 <i className="fas fa-user"></i>
@@ -152,15 +168,15 @@ export default function EmployeeForm(props) {
             <label className="label">Last Name</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                defaultValue={props.lastName}
-                onChange={lastNameUpdateChangeHandler}
-                id="last_name_admin_view"
-                className="input is-link"
-                type="text"
-                placeholder="Employees last name"
-                name="lastName"
-                required
-                pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+"
+                  defaultValue={this.state.lastName}
+                  onChange={this.lastNameUpdateChangeHandler}
+                  id="last_name_admin_view"
+                  className="input is-link"
+                  type="text"
+                  placeholder="Employees last name"
+                  name="lastName"
+                  required
+                  pattern="\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+"
               />
               <span className="icon is-small is-left">
                 <i className="fas fa-user"></i>
@@ -171,16 +187,16 @@ export default function EmployeeForm(props) {
             <label className="label">Date of Birth</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                defaultValue={props.dateOfBirth}
-                onChange={dateUpdateChangeHandler}
-                id="dataOfBirth_admin_view"
-                className="input is-link"
-                type="date"
-                name="DOB"
-                min="1970-01-01"
-                max="2021-12-31"
-                data-bouncer-message="Valid date range is from 1960 till current"
-                required
+                  defaultValue={this.state.dateOfBirth}
+                  onChange={this.dateUpdateChangeHandler}
+                  id="dataOfBirth_admin_view"
+                  className="input is-link"
+                  type="date"
+                  name="DOB"
+                  min="1970-01-01"
+                  max="2021-12-31"
+                  data-bouncer-message="Valid date range is from 1960 till current"
+                  required
               />
               <span className="icon is-small is-left">
                 <i className="fas fa-user"></i>
@@ -191,13 +207,13 @@ export default function EmployeeForm(props) {
             <label className="label">Email</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                defaultValue={props.email}
-                onChange={emailUpdateChangeHanndler}
-                className="input is-link"
-                id="email_admin_view"
-                type="email"
-                name="email"
-                required
+                  defaultValue={this.state.email}
+                  onChange={this.emailUpdateChangeHandler}
+                  className="input is-link"
+                  id="email_admin_view"
+                  type="email"
+                  name="email"
+                  required
               />
               <span className="icon is-small is-left">
                 <i className="fas fa-envelope"></i>
@@ -208,15 +224,15 @@ export default function EmployeeForm(props) {
             <label className="label">Phone Number</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                defaultValue={props.phone_number}
-                onChange={phoneNumberUpdateChangeHandler}
-                id="phone_number_admin_view"
-                className="input is-link"
-                type="number"
-                name="phone_number"
-                pattern="^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}( |-){0,1}[0-9]{2}( |-){0,1}[0-9]{2}( |-){0,1}[0-9]{1}( |-){0,1}[0-9]{3}$"
-                data-bouncer-message="Please use only Australian domestic numbers"
-                required
+                  defaultValue={this.state.phone_number}
+                  onChange={this.phoneNumberUpdateChangeHandler}
+                  id="phone_number_admin_view"
+                  className="input is-link"
+                  type="number"
+                  name="phone_number"
+                  pattern="^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}( |-){0,1}[0-9]{2}( |-){0,1}[0-9]{2}( |-){0,1}[0-9]{1}( |-){0,1}[0-9]{3}$"
+                  data-bouncer-message="Please use only Australian domestic numbers"
+                  required
               />
               <span className="icon is-small is-left">
                 <i className="fas fa-phone"></i>
@@ -227,15 +243,15 @@ export default function EmployeeForm(props) {
             <label className="label">Clock On Number</label>
             <div className="control has-icons-left has-icons-right">
               <input
-                defaultValue={props.clock_Number}
-                onChange={clockNumberUpdateChangeHandler}
-                className="input is-link"
-                id="clock_Number_admin_view"
-                type="number"
-                data-bouncer-message="Clock number needs to be 4 digits in length"
-                name="clockInNum"
-                required
-                maxLength="4"
+                  defaultValue={this.state.clock_Number}
+                  onChange={this.clockNumberUpdateChangeHandler}
+                  className="input is-link"
+                  id="clock_Number_admin_view"
+                  type="number"
+                  data-bouncer-message="Clock number needs to be 4 digits in length"
+                  name="clockInNum"
+                  required
+                  maxLength="4"
               />
               <span className="icon is-small is-left">
                 <i className="fas fa-user"></i>
@@ -247,10 +263,10 @@ export default function EmployeeForm(props) {
             <div className="control">
               <div className="select">
                 <select
-                  onChange={departmentUpdateChangeHandler}
-                  defaultValue={props.department}
-                  id="department_admin_view"
-                  name="department"
+                    onChange={this.departmentUpdateChangeHandler}
+                    defaultValue={this.state.department}
+                    id="department_admin_view"
+                    name="department"
                 >
                   <option disabled value="DEFAULT">
                     {" "}
@@ -269,10 +285,10 @@ export default function EmployeeForm(props) {
             <div className="control">
               <div className="select">
                 <select
-                  onChange={is_managerUpdateChangeHandler}
-                  defaultValue={props.is_manager}
-                  id="manager_admin_view"
-                  name="is_manager"
+                    onChange={this.is_managerUpdateChangeHandler}
+                    defaultValue={this.state.is_manager}
+                    id="manager_admin_view"
+                    name="is_manager"
                 >
                   {" "}
                   <option value="DEFAULT" disabled>
@@ -285,11 +301,13 @@ export default function EmployeeForm(props) {
               </div>
             </div>
           </div>
-          <button onClick={updateMyDetailsViewForm} className="button is-link">
+          <button onClick={this.updateMyDetailsViewForm} className="button is-link">
             Save changes
           </button>
         </form>
       </section>
-    </div>
-  );
+    </div>)
+  };
 }
+
+export default EmployeeForm;
