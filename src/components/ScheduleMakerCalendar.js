f1a631c29f123083c719494b0cf44cfa10e09725
cb91-dev <craigrbennett91@gmail.com>
2021-11-07 21:16:34 +1000

    Sunday night

--- src/components/ScheduleMakerCalendar.js
+++ src/components/ScheduleMakerCalendar.js
@@ -14,28 +14,26 @@ export default class ScheduleMaker extends React.Component {
       items: [],
       isLoaded: false,
       user: props.employees_idNumber,
+      renderData: props.renderData,
     };
   }
+  calendarRef = React.createRef(null);
 
-  componentDidMount() {
-    fetch("http://localhost:8888/api/api.php?action=viewFullSchedule")
-      .then((res) => res.json())
-      .then((result) => {
-        this.setState({
-          isLoaded: true,
-          items: result,
-        });
-      });
+  onShiftAdded() {
+    let a = this.props.renderData;
+    console.log(a);
+    let calendarApi = this.calendarRef.current.getApi();
+    calendarApi.addEvent(a);
   }
 
   render() {
-    console.log(this.state.items);
     return (
       <div className="panel has-background-white">
+        <button onClick={this.onShiftAdded}></button>
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
           // dateClick={this.handleDateClick}
-          events={junk}
+          ref={this.calendarRef}
           select={this.handleDateSelect}
           dateClick={this.sendingDateDataToForm}
           initialView="dayGridMonth"
@@ -43,19 +41,4 @@ export default class ScheduleMaker extends React.Component {
       </div>
     );
   }
-  handleDateSelect = (selectInfo) => {
-    let title = prompt("Please enter a new title for your event");
-    let calendarApi = selectInfo.view.calendar;
-
-    calendarApi.unselect(); // clear date selection
-
-    if (title) {
-      calendarApi.addEvent({
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      });
-    }
-  };
 }
